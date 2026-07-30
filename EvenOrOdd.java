28af808d44e245f4cfc3a9aafcd42f3ad06993f0
Pratyush Singh <prs015@ucsd.edu>
2026-07-30 07:45:54 -0700

    Even or Odd problem

---
+++ EvenOrOdd.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class EvenOrOdd{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);   
+        System.out.print("Enter your number: "); 
+        int number = sc.nextInt();
+        if(number % 2 == 0){
+            System.out.println(number + " is even!");
+        }
+        else{
+            System.out.println(number + " is odd!");
+        }
+    }
+}
